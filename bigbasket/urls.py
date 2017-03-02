4d35c1087ebf60d1c2475d7030243783d91b62ef
Nitin Chavan <initinbc@gmail.com>
2017-03-02 21:49:44 +0530

    primary view

--- bigbasket/urls.py
+++ bigbasket/urls.py
@@ -1,4 +1,4 @@
-"""bigbasket URL Configuration
+"""bigbasket2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -14,13 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.conf.urls.static import static
-from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+# from items import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.login,{'template_name':'login.html'}, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url(r'^items/', include('items.urls')),
+    url(r'^$',RedirectView.as_view(url='/items/', permanent=True) ),
 ]
 
-#Include Media and Static Files
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- items/views.py
+++ items/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Item, Category, Price
 # Create your views here.
+
+@login_required
+def home(request):
+	items_list = Item.objects.all()
+	
+	page = request.GET.get('page', 1)
+
+	paginator = Paginator(items_list, 10)
+	try:
+		items = paginator.page(page)		
+	except PageNotAnInteger:
+		items = paginator.page(1)
+	except EmptyPage:
+		items = paginator.page(paginator.num_pages)
+
+	return render(request, 'items/items_list.html', {'items':items})
